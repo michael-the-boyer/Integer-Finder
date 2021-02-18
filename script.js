992c66f26be6917977b1ac1d80401862f131580b
michaeltheboyer <michael.boyer@ocsbstudent.ca>
2021-02-18 22:03:23 +0000

    Third times a charm

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 document.getElementById('button').addEventListener('click', findInteger)
 
 function findInteger () {
-
   let number = document.getElementById('userinput').value
   number = parseInt(number)
-
   let type = 'an integer.'
-
   if (number < 0) {
     type = 'negative.'
   } else if (number > 0) {
@@ -14,6 +11,5 @@ function findInteger () {
   } else {
     type = 'either not a proper number or has a value of 0.'
   }
-
   document.getElementById('answertext').innerHTML = 'The number ' + Math.floor(number) + ' is ' + type
 }
