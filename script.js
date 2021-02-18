1fbf6b41b280f972761821bc58807223409d48d0
michaeltheboyer <michael.boyer@ocsbstudent.ca>
2021-02-18 21:54:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('button').addEventListener('click', findInteger)
+
+function findInteger () {
+
+  let number = document.getElementById('userinput').value
+  number = parseInt(number)
+
+  let type = "an integer."
+
+  if (number < 0) {
+    type = "negative."
+  } else if (number > 0) {
+    type = "positive."
+  } else {
+    type = "either not a proper number or has a value of 0."
+  }
+
+  document.getElementById('answertext').innerHTML="The number " + Math.floor(number) + " is " + type
+}
